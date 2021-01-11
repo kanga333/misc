24dfbfb7618b66fc90827d84817ca0515ff79852
kanga333 <e411z7t40w@gmail.com>
2021-01-11 16:03:03 +0900

    ポインタ実装にする

--- プログラミングコンテスト攻略のためのアルゴリズムとデータ構造/src/part8/8.2/main.go
+++ プログラミングコンテスト攻略のためのアルゴリズムとデータ構造/src/part8/8.2/main.go
@@ -10,21 +10,25 @@ import (
 )
 
 type tree struct {
-	id     int
-	parent *tree
-	child  []int
+	id        int
+	depth     int
+	parent    *tree
+	child     *tree
+	lastChild *tree
+	sibling   *tree
 }
 
-func (t *tree) updateParent(p *tree) {
-	t.parent = p
-}
+func (t *tree) appendChild(c *tree) {
+	c.parent = t
 
-func (t *tree) updateChild(c []int, register []*tree) {
-	t.child = c
-	for _, cid := range c {
-		cp := register[cid]
-		cp.updateParent(t)
+	if t.child == nil {
+		t.child = c
+		t.lastChild = c
+		return
 	}
+
+	t.lastChild.sibling = c
+	t.lastChild = c
 }
 
 func (t *tree) pID() int {
@@ -34,17 +38,19 @@ func (t *tree) pID() int {
 	return t.parent.id
 }
 
-func (t *tree) depth() int {
-	if t.parent == nil {
-		return 0
-	}
-	return t.parent.depth() + 1
-}
-
 func (t *tree) childStr() string {
-	s := []string{}
-	for _, c := range t.child {
-		s = append(s, strconv.Itoa(c))
+	c := t.child
+	if c == nil {
+		return "[]"
+	}
+
+	s := []string{strconv.Itoa(c.id)}
+	for {
+		c = c.sibling
+		if c == nil {
+			break
+		}
+		s = append(s, strconv.Itoa(c.id))
 	}
 	return fmt.Sprintf("[%s]", strings.Join(s, ", "))
 }
@@ -53,19 +59,27 @@ func (t *tree) nodeType() string {
 	if t.parent == nil {
 		return "root"
 	}
-	if len(t.child) == 0 {
+	if t.child == nil {
 		return "leaf"
 	}
 	return "internal node"
+}
+
+func (t *tree) calcDepth(depth int) {
+	t.depth = depth
+	if t.child != nil {
+		t.child.calcDepth(depth + 1)
+	}
+	if t.sibling != nil {
+		t.sibling.calcDepth(depth)
+	}
 }
 
 func initTrees(n int) []*tree {
 	trees := make([]*tree, n, n)
 	for i := 0; i < n; i++ {
 		trees[i] = &tree{
-			id:     i,
-			parent: nil,
-			child:  []int{},
+			id: i,
 		}
 	}
 	return trees
@@ -82,16 +96,22 @@ func main() {
 		id := params[0]
 		t := trees[id]
 		degree := params[1]
-		child := []int{}
 		for i := 0; i < degree; i++ {
-			child = append(child, params[i+2])
+			c := trees[params[i+2]]
+			t.appendChild(c)
 		}
-		t.updateChild(child, trees)
+	}
+
+	for _, t := range trees {
+		if t.nodeType() == "root" {
+			t.calcDepth(0)
+			break
+		}
 	}
 
 	for _, t := range trees {
 		fmt.Printf("node %d: parent = %d, depth = %d, %s, %v\n",
-			t.id, t.pID(), t.depth(), t.nodeType(), t.childStr())
+			t.id, t.pID(), t.depth, t.nodeType(), t.childStr())
 	}
 }
 
