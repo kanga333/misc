349f22b07df40f980571e54429ecb9163ef13536
kanga333 <e411z7t40w@gmail.com>
2021-04-27 22:55:00 +0900

    classによる実装

--- aoj/src/ALDS1_3_C/main.py
+++ aoj/src/ALDS1_3_C/main.py
@@ -1,29 +1,98 @@
-from collections import deque
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.prev = None
+        self.next = None
+    
+    def set_next(self, x):
+        self.next = x
+    
+    def set_prev(self, x):
+        self.prev = x
+
+
+class NodeQueue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+    
+    def delete_first(self):
+        if self.first:
+            next = self.first.next
+            if next:
+                next.set_prev(None)
+            self.first = next
+        
+        if self.first == None:
+            self.last = None
+    
+    def delete_last(self):
+        if self.last:
+            prev = self.last.prev
+            if prev:
+                prev.set_next(None)
+            self.last = prev
+        
+        if self.last == None:
+            self.first = None
+    
+    def insert(self, x):
+        n = Node(x)
+        n.set_next(self.first)
+        if self.first:
+            self.first.set_prev(n)
+        self.first = n
+
+        if self.last == None:
+            self.last = n
+    
+    def delete(self, x):
+        node = self.first
+        while node != None:
+            key = node.key
+            if key == x:
+                prev = node.prev
+                next = node.next
+                if prev:
+                    prev.set_next(next)
+                else:
+                    self.first = next
+                if next:
+                    next.set_prev(prev)
+                else:
+                    self.last = prev
+                break
+            node = node.next
+    
+    def print(self):
+        node = self.first
+        result = []
+        while node != None:
+            result.append(node.key)
+            node = node.next
+        print(" ".join(result))
 
 # ALDS1_3_C: 連結リスト
 def main():
-    l = deque()
+    l = NodeQueue()
     n = int(input())
     for i in range(0, n):
         cmd = input()
         if cmd == "deleteFirst":
-            _ = l.popleft()
+            l.delete_first()
             continue
         if cmd == "deleteLast":
-            _ = l.pop()
+            l.delete_last()
             continue
         splited = cmd.split(" ")
         target = splited[1]
         if splited[0] == "delete":
-            try:
-                l.remove(target)
-            except ValueError:
-                pass
+            l.delete(target)
             continue
         # insert
-        l.appendleft(target)
+        l.insert(target)
     
-    print(" ".join(list(l)))
+    l.print()
 
 if __name__ == '__main__':
     main()
