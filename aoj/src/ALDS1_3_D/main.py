cb51bbf5936c9b7df7d120b7def77957bbb2cbc3
kanga333 <e411z7t40w@gmail.com>
2021-05-01 14:36:34 +0900

    enumerateとreversedを使用

--- aoj/src/ALDS1_3_D/main.py
+++ aoj/src/ALDS1_3_D/main.py
@@ -1,36 +1,33 @@
 # ALDS1_3_D: 面積計算
 def main():
-    x = 0
     slopes = []
     pools = []
     sum = 0
     lines = input()
 
-    for c in lines:
-        if c == "\\":
+    for x, char in enumerate(lines):
+        if char == "\\":
             slopes.append(x)
-        if c == "/":
+        if char == "/":
             if len(slopes) == 0:
-                x += 1
                 continue
             x0 = slopes.pop()
             area = x - x0
             sum += area
             merge_num = 0
-            for i in range(len(pools)-1, -1, -1):
+            for i in reversed(range(0, len(pools))):
                 if pools[i]['x0'] < x0:
                     break
                 merge_num += 1
-            for i in range(0, merge_num):
+            for _ in range(0, merge_num):
                 area += pools.pop()['area']
             pools.append({'x0': x0, 'area': area})
-        x += 1
     
     print(sum)
-    if len(pools) == 0:
-        print(0)
-    else:
-        print(len(pools)," ".join([str(p['area']) for p in pools]))
+    print(len(pools), end="")
+    if len(pools) != 0:
+        print(" ", end="")
+        print(" ".join([str(p['area']) for p in pools]))
 
 if __name__ == '__main__':
     main()
