8657a27d32d686283bfd00ea4820ab2e82aac122
kanga333 <e411z7t40w@gmail.com>
2021-05-10 13:05:29 +0900

    答えをメモリに持たせる

--- aoj/src/ALDS1_5_A/main.py
+++ aoj/src/ALDS1_5_A/main.py
@@ -1,11 +1,13 @@
-def exhaustive_search(target, input):
-    if target == 0:
-        return True
+ans = {}
+
+def exhaustive_search(current, input):
+    global ans
     if len(input) == 0:
         return False
-    if input[0] <= target:
-        return exhaustive_search(target - input[0], input[1:]) or exhaustive_search(target, input[1:])
-    return exhaustive_search(target, input[1:])
+    next = current + input[0]
+    ans[next] = True
+    exhaustive_search(next, input[1:])
+    exhaustive_search(current, input[1:])
     
     
 
@@ -15,8 +17,11 @@ def main():
     s = [int(v) for v in input().split(" ")]
     _ = input()
     q = [int(v) for v in input().split(" ")]
+    global ans
+    ans[0] = True
+    exhaustive_search(0, s)
     for qv in q:
-        if exhaustive_search(qv, s):
+        if qv in ans:
             print("yes")
         else:
             print("no")
