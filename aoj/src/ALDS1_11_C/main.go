28d1fb6e4ea21d2c86a758021a0b03a317520bf4
kanga333 <e411z7t40w@gmail.com>
2021-07-15 21:41:14 +0900

    mv

--- aoj/src/ALDS1_11_C/main.go
+++ aoj/src/ALDS1_11_C/main.go
@@ -9,44 +9,26 @@ import (
 	"strings"
 )
 
-const max = 1001
+var (
+	d []int
+)
 
-type dpLcs struct {
-	c [][]int
-}
-
-func newDpLcs() *dpLcs {
-	c := make([][]int, max, max)
-	for i := range c {
-		c[i] = make([]int, max, max)
-	}
-	return &dpLcs{
-		c: c,
-	}
-}
-
-func (dp *dpLcs) lcs(x, y string) int {
-	val := 0
-	for i, xi := range x {
-		for j, yj := range y {
-			if xi == yj {
-				dp.c[i+1][j+1] = dp.c[i][j] + 1
-			} else if dp.c[i][j+1] >= dp.c[i+1][j] {
-				dp.c[i+1][j+1] = dp.c[i][j+1]
-			} else {
-				dp.c[i+1][j+1] = dp.c[i+1][j]
-			}
-			if val < dp.c[i+1][j+1] {
-				val = dp.c[i+1][j+1]
+func visit(t int, us []int, adj [][]int) {
+	next := []int{}
+	for _, u := range us {
+		for i, v := range adj[u] {
+			if v == 1 && d[i] == -1 {
+				d[i] = t
+				next = append(next, i)
 			}
 		}
-
 	}
-
-	return val
+	if len(next) != 0 {
+		visit(t+1, next, adj)
+	}
 }
 
-// ALDS1_10_C: LCS
+// ALDS1_11_C: 幅優先探索
 func main() {
 	stdin := bufio.NewScanner(os.Stdin)
 	stdin.Buffer([]byte{}, math.MaxInt64)
@@ -54,15 +36,30 @@ func main() {
 	defer w.Flush()
 
 	n := scanInt(stdin)
+	adj := make([][]int, n, n)
 	for i := 0; i < n; i++ {
-		stdin.Scan()
-		x := stdin.Text()
-		stdin.Scan()
-		y := stdin.Text()
-		dp := newDpLcs()
-		fmt.Fprintf(w, "%d\n", dp.lcs(x, y))
-	}
+		list := make([]int, n, n)
+		a := scanIntArray(stdin)
+		// 頂点
+		u := a[0] - 1
+		// 隣接する頂点
+		vs := a[2:]
 
+		for _, v := range vs {
+			list[v-1] = 1
+		}
+		adj[u] = list
+	}
+	d = make([]int, n, n)
+	for i := range d {
+		d[i] = -1
+	}
+	d[0] = 0
+	visit(1, []int{0}, adj)
+
+	for i := 0; i < n; i++ {
+		fmt.Fprintf(w, "%d %d\n", i+1, d[i])
+	}
 }
 
 func scanInt(sc *bufio.Scanner) int {
